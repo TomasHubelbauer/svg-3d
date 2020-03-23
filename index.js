2a6cffecdf7d3a9a68605a132e1a9d6476a6d60a
Tomas Hubelbauer <tomas@hubelbauer.net>
2020-03-23 21:25:04 +0100

    Implement the initial proof of concept

---
+++ index.js
@@ -0,0 +1,93 @@
+window.addEventListener('load', () => {
+  const canvasSvg = document.getElementById('canvasSvg');
+
+  const width = 640;
+  canvasSvg.setAttribute('width', width);
+
+  const height = 480;
+  canvasSvg.setAttribute('height', height);
+
+  const size = Math.min(width, height) / 2;
+
+  const step1 = Math.PI / 2;
+  const step2 = Math.PI;
+  const step3 = Math.PI * 1.5;
+
+  function* render() {
+    const t = window.performance.now() / 1000;
+    const s = Math.abs(Math.sin(t));
+
+    const leftTopBack = [Math.sin(t) * s, Math.cos(t) * s, -10];
+    const leftTopFront = [Math.sin(t) * s, Math.cos(t) * s, -10 + s * 3];
+    const leftBottomBack = [Math.sin(t + step1) * s, Math.cos(t + step1) * s, -10];
+    const leftBottomFront = [Math.sin(t + step1) * s, Math.cos(t + step1) * s, -10 + s * 3];
+    const rightBottomBack = [Math.sin(t + step2) * s, Math.cos(t + step2) * s, -10];
+    const rightBottomFront = [Math.sin(t + step2) * s, Math.cos(t + step2) * s, -10 + s * 3];
+    const rightTopBack = [Math.sin(t + step3) * s, Math.cos(t + step3) * s, -10];
+    const rightTopFront = [Math.sin(t + step3) * s, Math.cos(t + step3) * s, -10 + s * 3];
+
+    const polylines = [
+      [...leftTopBack, ...leftTopFront],
+      [...leftBottomBack, ...leftBottomFront],
+      [...rightBottomBack, ...rightBottomFront],
+      [...rightTopBack, ...rightTopFront],
+      [...leftTopBack, ...leftBottomBack, ...rightBottomBack, ...rightTopBack],
+      [...leftTopFront, ...leftBottomFront, ...rightBottomFront, ...rightTopFront],
+    ];
+
+    for (const polyline of polylines) {
+      const points = polyline.reduce((a, c, i, s) => i % 3 === 0 ? [...a, s.slice(i, i + 3)] : a, []);
+
+      // Make the polyline self-meeting
+      points.push(points[0]);
+
+      let polylinePoints = '';
+      for (const point of points) {
+        const x = (width / 2) + (point[0] * (5 / point[2])) * size;
+        const y = (height / 2) + (point[1] * (5 / point[2])) * size;
+        polylinePoints += x + ',' + y + ' ';
+      }
+
+      yield polylinePoints;
+    }
+  }
+
+  function mount() {
+    const polylines = render();
+    const fragment = document.createDocumentFragment();
+    for (const polyline of polylines) {
+      const polylinePolyline = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
+      polylinePolyline.setAttribute('points', polyline);
+      polylinePolyline.setAttribute('stroke', 'black');
+      polylinePolyline.setAttribute('fill', 'none');
+
+      fragment.append(polylinePolyline);
+    }
+
+    canvasSvg.append(fragment);
+  }
+
+  function reconcile() {
+    const polylines = render();
+    let index = 0;
+    for (const polyline of polylines) {
+      const polylinePolyline = canvasSvg.children[index];
+      if (!polylinePolyline || polylinePolyline.tagName !== 'polyline') {
+        throw new Error('The DOM has changed unexpectedly.');
+      }
+
+      polylinePolyline.setAttribute('points', polyline);
+      index++;
+    }
+
+    if (canvasSvg.children.length !== index) {
+      throw new Error('The DOM has changed unexpectedly.');
+    }
+  }
+
+  mount();
+  window.requestAnimationFrame(function loop() {
+    reconcile();
+    window.requestAnimationFrame(loop);
+  });
+});
---
+++ readme.md
@@ -0,0 +1,43 @@
+# SVG 3D
+
+Primitive 3D wireframe model rendering using SVG polylines.
+
+This is a proof of concept for rendering 3D wireframe models using SVG. I made
+it to see if it could be applicable for a problem I'm solving: anymating 3D
+wireframe models and scenes, which are static in the amount of vertices, edges
+and faces, but dynamic in terms of animation.
+
+It seems to work well for this, but I have made no comparisons to WebGL and
+`canvas` based implementations. I am not sure if `canvas` is GPU-accelerated,
+nor if SVG is, so if it is, it will be at best the same as WebGL, if it isn't,
+it will be way worse (CPU rendering).
+
+The only selling point of this solution is that it limits itself to wireframes,
+not shaded models. This is helpful, because once the polylines of the edges are
+mounted in the DOM, the DOM layout within the SVG remains constant and the only
+thing that changes are the `points` attributes (and possibly others later), so
+the DOM does not have to work anymore, only the vector rasterizer.
+
+## Maintenance Status
+
+Not actively maintained.
+
+## Similar Projects
+
+There is [Z-Dog](https://zzz.dog/) and also a lot of other people's pet projects
+having to do with SVG and 3D.
+
+## To-Do
+
+### Implement camera position and globe gizmo for scene navigation
+
+Right now the camera is stuck at the origin and the meshes have to transform.
+
+### Implement matrix transforms to make it easy to translate, scale and rotate meshes
+
+This will make it easy to tesselate meshes and then lay them out in the scene.
+
+### Implement rendering Wavefront model files as wireframes
+
+This will make it possible to render models designed in Blender and exported to
+Wavefront.
